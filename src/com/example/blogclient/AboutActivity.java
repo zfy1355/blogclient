ceebb62c86940857018f08c83a58035afa98a90f
fengyang.zhang <fengyang.zhang@busonline.com>
2016-03-15 18:38:07 +0800

    init code

---
+++ src/com/example/blogclient/AboutActivity.java
@@ -0,0 +1,32 @@
+package com.example.blogclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+import com.example.helloworld.R;
+
+public class AboutActivity extends Activity {
+	private ImageView backBtn;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+		backBtn = (ImageView) findViewById(R.id.backBtn);
+		
+		backBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				finish();
+			}
+		});
+		
+		
+	}
+}
